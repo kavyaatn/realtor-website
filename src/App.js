a0eeecb8e2c09f7b7b90b718a603ce9c7d8ab8bc
kavya <kavyanagaraj15@gmail.com>
2024-11-15 02:50:29 +0530

    pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />        
+      </Routes>
+    </Router>
+    </>
   );
 }
 
